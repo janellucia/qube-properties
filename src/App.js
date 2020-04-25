cf4144ecdce2eebf3ae8940ef7f9e6ee132429d8
janel lucia <thejanellucia@gmail.com>
2020-04-25 16:01:31 -0400

    user tracking

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
   return (
     <div>
       <Concepts />
+      <script>var clicky_site_ids = clicky_site_ids || []; clicky_site_ids.push(101250406);</script>
+      <script async src="//static.getclicky.com/js"></script>
+      <noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101250406ns.gif" /></p></noscript>
     </div>
   );
 }
