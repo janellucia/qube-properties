32c2f6cc7223af98faa0de9b72a4208a5cffc557
janel lucia <thejanellucia@gmail.com>
2020-08-11 14:42:31 -0400

    revert to commit adf1ac6a606c49822d5e611b941a30f1d04dd86e april 26 2020

--- src/pages/Values.js
+++ src/pages/Values.js
@@ -10,6 +10,8 @@ import positioning from '../images/concepts/values/statement.png';
 function Values() {
   return (
     <div className="values">
+      <h3>Brand Values</h3>
+      <p>I always start the process with a kick-off meeting to get to know your values in your brand, from there, I conduct my research, crafting the brand values, positioning statement, and user profiles. Then, I'll develop a brand identity system that includes a logo, colour palette, a bold and flexible font pairing, assets for your social channels, and brand guidelines. This will help the design process stay informed throughout.</p>
       <img src={positioning} alt="brand statement" />
       <img src={principles} alt="brand design principles" />
       <img src={personaOne} alt="brand user persona Michael" />
